39fe3d777011868ca76b7cdfc61bd7e64e7274a2
jayhatha <jay@jayhathaway.com>
2018-06-14 12:59:57 -0700

    added all api routes and ejs files

--- index.js
+++ index.js
@@ -20,23 +20,78 @@ app.set('view engine', 'ejs');
 // Routes begin here
 
 // | GET | /games | index | display a list of all games |
-app.get('/', function(req, res) {
-  // this will error out, navigate to your /games route and read the error.
-  // What is missing in your views directory?
-  res.render('games');
+app.get('/games', function(req, res) {
+      var fileContents = fs.readFileSync('./games.json');
+      var games = JSON.parse(fileContents);
+      res.render('index', {games: games, id: req.params.name});
 });
 
 // | GET | /games/new | new | return an HTML form for creating a new game |
+app.get('/games/new', function(req, res) {
+      res.render('new');
+  });
+
 
 // | POST | /games | create | create a new game (using form data from /games/new) |
 
+app.post('/games', function(req, res) {
+    var games = fs.readFileSync('./games.json');
+    games = JSON.parse(games);
+    games.push({name: req.body.name, description: req.body.description});
+    fs.writeFileSync('./games.json', JSON.stringify(games));
+    res.redirect('/games');
+});
+
+
 // | GET | /games/:name | show | display a specific game |
+
+app.get("/games/:name", function(req, res) {
+  // returns one game
+  var games = fs.readFileSync("games.json");
+  games = JSON.parse(games);
+  var name = req.params.name;
+    res.render('show', {game: games[req.params.name]});
+  });
 
 // | GET | /games/:name/edit | edit | return an HTML form for editing a game |
 
+app.get('/games/:name/edit', function(req, res) {
+  var games = fs.readFileSync("games.json");
+games = JSON.parse(games);
+res.render('edit', {game: games[req.params.name], id: req.params.name});
+  });
+
+
 // | PUT | /games/:name | update | update a specific game (using form data from /games/:name/edit) |
 
+app.put("/games/:name", function(req, res) {
+  // returns all rappers
+  var Games = fs.readFileSync("games.json");
+  games = JSON.parse(games);
+  var gameIndex = req.params.name;
+  var gameName = req.body.name;
+  var gameDesc = req.body.description;
+  if (mcIndex >= mcs.length) {
+    res.send('error');
+  } else {
+  games.splice(mcIndex, 1, {name: gameName, albums: gameDesc} );
+  fs.writeFileSync("./games.json", JSON.stringify(games));
+  res.json(games);
+}
+});
+
+
 // | DELETE | /games/:name | destroy | deletes a specific game |
+
+app.delete("/games/:name", function(req, res) {
+  // deletes a game
+  var games = fs.readFileSync("./games.json");
+  games = JSON.parse(games);
+  games.splice(req.params.name, 1);
+  fs.writeFileSync("./games.json", JSON.stringify(games));
+  res.json('games');
+});
+
 
 
 // helper functions
--- static/js/main.js
+++ static/js/main.js
@@ -4,9 +4,35 @@ console.log("JS good to go, sir!");
 // at the path GET /games/:name/edit. Use AJAX to send a PUT request to the
 // appropriate URL
 
-// code here ...
+$(document).ready(function() {
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        var newData = $(this).serialize();
+        var url = $(this).attr('action');
+        console.log(url);
+        $.ajax({
+            method: 'PUT',
+            url: url,
+            data: newData
+        }).done(function(data) {
+            console.log(data);
+            window.location = '/games';
+        });
+    });
+
 
 // listen for clicks on "delete" links. use AJAX to send a DELETE HTTP request
 // to the appropriate URL
 
-// code here ...
+
+    $('a').on('click', function(event) {
+      event.preventDefault();
+      var url = $(this).attr('href');
+      $.ajax({
+        method: 'DELETE',
+        url: url
+      }).done(function(data) {
+        window.location = '/games';
+      });
+    });
+});
